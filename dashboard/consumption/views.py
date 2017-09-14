8133da647a039e6d7f9d4a6540c471409d3cf38b
phoenix <phoenixkrypt@gmail.com>
2017-09-14 15:34:14 +0100

    Updated summary view
    - Implemented aggregation for consumption data
    - Added functionality for consumption data charts
    - Added gathering data for user tables

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -2,16 +2,49 @@
 from __future__ import unicode_literals
 
 from django.shortcuts import render
+from django.db.models import Avg, Sum
 
-# Create your views here.
+from models import UserData, ConsumptionData
 
+from graphos.sources.model import SimpleDataSource
+from graphos.renderers.gchart import LineChart, AreaChart
+
+# Function to calculate average and total consumption
+def aggregate_consumption():
+    distinct_dates = sorted(set(each[0].date() for each in ConsumptionData.objects.values_list('dateinfo').distinct()))
+
+    # Average and total consumption of all users in a day
+    consumption = [ConsumptionData.objects.filter(dateinfo__startswith=each).aggregate(agg=Avg('consumption'), tot=Sum('consumption')) for each in distinct_dates]
+    avg_consumption = [each['agg'] for each in consumption]
+    tot_consumption = [each['tot'] for each in consumption]
+
+    return list(distinct_dates), avg_consumption, tot_consumption
 
 def summary(request):
-    context = {
-        'message': 'Hello!',
-    }
-    return render(request, 'consumption/summary.html', context)
+    xdata, avgdata, totdata = aggregate_consumption()
 
+    # LineChart for average consumption
+    data = [['Date', 'Average consumption']] + [[x, av] for x, av in zip(xdata, avgdata)]
+    avgchart = LineChart(SimpleDataSource(data=data), options={'title': \
+                      'Average Energy Consumption of All Users from %s to %s' \
+                      %(xdata[0], xdata[-1]), 'legend': {'position': 'top'}, \
+                      'vAxes': {0: {'title': 'Average energy consumption (Wh)'}},\
+                      'hAxes': {0: {'title': 'Date energy consumption data collected'}}})
+
+    # AreaChart for total consumption
+    data = [['Date', 'Total consumption']] + [[x, tot] for x, tot in zip(xdata, totdata)]
+    totchart = AreaChart(SimpleDataSource(data=data), options={'title': \
+                      'Total Energy Consumption of All Users from %s to %s' \
+                      %(xdata[0], xdata[-1]), 'legend': {'position': 'top'}, \
+                      'vAxes': {0: {'title': 'Total energy consumption (Wh)'}}, \
+                      'hAxes': {0: {'title': 'Date energy consumption data collected'}},\
+                      'colors': ['green']})
+
+    # Data for user data table
+    users = UserData.objects.order_by('userid')
+
+    context = {'avgchart': avgchart, 'totchart': totchart, 'users': users}
+    return render(request, 'consumption/summary.html', context)
 
 def detail(request):
     context = {
