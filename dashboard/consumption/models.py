6c827699afebf4551904b13aca28768baa489ef5
phoenix <phoenixkrypt@gmail.com>
2017-09-14 15:29:53 +0100

    Added the models for User adn Consumption data

--- dashboard/consumption/models.py
+++ dashboard/consumption/models.py
@@ -3,4 +3,12 @@ from __future__ import unicode_literals
 
 from django.db import models
 
-# Create your models here.
+class UserData(models.Model):
+    userid = models.IntegerField(primary_key=True, unique=True)
+    area = models.CharField(max_length=150)
+    tariff = models.CharField(max_length=150)
+
+class ConsumptionData(models.Model):
+    dateinfo = models.DateTimeField()
+    consumption = models.FloatField()
+    user = models.ForeignKey(UserData, to_field='userid', on_delete=models.CASCADE)
