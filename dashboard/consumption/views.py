db60927416529f437d237781848342ae1f9fbaee
phoenix <phoenixkrypt@gmail.com>
2017-09-15 11:41:08 +0100

    Updated aggregate function, chart and modified html
    - views.py: Updated aggregate_consumption to be faster and changed the total
                consumption to LineChart and average consumption to AreaChart
    - layout.html: Added heading
    - summary.html: Swapped charts display

--- dashboard/consumption/views.py
+++ dashboard/consumption/views.py
@@ -3,6 +3,7 @@ from __future__ import unicode_literals
 
 from django.shortcuts import render
 from django.db.models import Avg, Sum
+from django.db.models.functions import TruncDate
 
 from models import UserData, ConsumptionData
 
@@ -16,33 +17,28 @@ def mainpage(request):
 
 # Function to calculate average and total consumption
 def aggregate_consumption():
-    distinct_dates = sorted(set(each[0].date() for each in ConsumptionData.objects.values_list('dateinfo').distinct()))
-
     # Average and total consumption of all users in a day
-    consumption = []
-    for each in distinct_dates:
-        aggr = ConsumptionData.objects.filter(dateinfo__startswith=each).aggregate(agg=Avg('consumption'), tot=Sum('consumption'))
-        consumption.append(aggr)
+    consumption = ConsumptionData.objects.annotate(d=TruncDate('dateinfo')).values('d').annotate(agg=Avg('consumption'), tot=Sum('consumption'))
 
+    distinct_dates = [each['d'] for each in consumption]
     avg_consumption = [each['agg'] for each in consumption]
     tot_consumption = [each['tot'] for each in consumption]
-
-    return list(distinct_dates), avg_consumption, tot_consumption
+    return distinct_dates, avg_consumption, tot_consumption
 
 def summary(request):
     xdata, avgdata, totdata = aggregate_consumption()
 
-    # LineChart for average consumption
+    # AreaChart for average consumption
     data = [['Date', 'Average consumption']] + [[x, av] for x, av in zip(xdata, avgdata)]
-    avgchart = LineChart(SimpleDataSource(data=data), options={'title': \
+    avgchart = AreaChart(SimpleDataSource(data=data), options={'title': \
                       'Average Energy Consumption of All Users from %s to %s' \
                       %(xdata[0], xdata[-1]), 'legend': {'position': 'top'}, \
                       'vAxes': {0: {'title': 'Average energy consumption (Wh)'}},\
                       'hAxes': {0: {'title': 'Date energy consumption data collected'}}})
 
-    # AreaChart for total consumption
+    # LineChart for total consumption
     data = [['Date', 'Total consumption']] + [[x, tot] for x, tot in zip(xdata, totdata)]
-    totchart = AreaChart(SimpleDataSource(data=data), options={'title': \
+    totchart = LineChart(SimpleDataSource(data=data), options={'title': \
                       'Total Energy Consumption of All Users from %s to %s' \
                       %(xdata[0], xdata[-1]), 'legend': {'position': 'top'}, \
                       'vAxes': {0: {'title': 'Total energy consumption (Wh)'}}, \
